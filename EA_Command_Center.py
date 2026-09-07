204aa4cb68e986036bb95003c2eef406c5596778
christina-sunil <cpriya@progress.com>
2026-09-07 15:08:29 +0530

    Update EA_Command_Center.py
    Add Backlog and Week in Review tabs

--- EA_Command_Center.py
+++ EA_Command_Center.py
@@ -6,3 +6,14 @@ st.set_page_config(
 )
 
 st.title("Enterprise Applications Command Center")
+
+tab1, tab2 = st.tabs([
+    "Backlog",
+    "Week in Review"
+])
+
+with tab1:
+    st.header("Backlog")
+
+with tab2:
+    st.header("Week in Review")
